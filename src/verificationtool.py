3684692e1b473808024374c858233b53d77259db
udqps <udqps@student.kit.edu>
2023-10-17 09:46:53 +0200

    Updated checkstyle to ignore spaces in blank line

--- src/verificationtool.py
+++ src/verificationtool.py
@@ -754,7 +754,7 @@ def verify_receipts(path, phase1=None, log=False, logTo=None):
         elif fingerprint:
             totalConfirmationsFound += 1
             ballotsByFingerprint[fingerprint] = None
-        
+
         if log and fingerprint in ballotsByFingerprint:
             status = ballots[ballotsByFingerprint[fingerprint]].status
             logger.info("Ballot %s is included in the ballot box with status %s." % (fingerprint, status))
